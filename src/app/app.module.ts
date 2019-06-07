db9155c4354b26d71730c1405f5959cc619c230d
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-07 10:29:29 +0300

    add stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -12,10 +13,11 @@ import { SchedulesService } from './schedules.service';
     AppComponent,
     FooterComponent,
     SidebarComponent,
-    ScheduleComponent
+    ScheduleComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [SchedulesService],
   bootstrap: [AppComponent]
