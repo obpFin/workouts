ac84c7c6ca3efe23ce21c0d49e6298a163f9496e
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-07 10:30:28 +0300

    addSchedule function

--- src/app/schedules.service.ts
+++ src/app/schedules.service.ts
@@ -1,17 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef } from '@angular/core';
+
+import { utils } from './shared/utils'
 
 @Injectable()
 export class SchedulesService {
-  schedules = [
+  selectedSchedule: number
+  schedules: {id: number, name: string}[] = [
     {
+      id: 1,
       name: 'sch 1',
     }, 
     {
+      id: 2,
       name: 'sch 2'
     }
   ]
 
-  addSchedule(name: string) {
-    this.schedules.push({name: name})
+  addSchedule(nameRef: ElementRef) {
+    const id = this.getId()
+    const name = nameRef.nativeElement.value
+    const schedule = {name, id}
+    this.schedules.push(schedule)
   }
+
+  selectSchedule(id: number) {
+    this.selectedSchedule = id
+  }
+
+  getId() {
+    let id = utils.randomNumber(1, 99)
+    if (this.schedules.some(s => s.id === id)) {
+      return this.getId()
+    }
+    return id
+  }
+
+
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { SchedulesService } from '../schedules.service';
 
 @Component({
@@ -6,14 +6,26 @@ import { SchedulesService } from '../schedules.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.sass']
 })
-export class SidebarComponent implements OnInit {
-  schedules: {name: string}[] = []
+export class SidebarComponent implements OnInit, OnChanges {
+  @ViewChild('scheduleName') scheduleName: ElementRef;
+  scheduleLinks: {id: number, name: string}[] = []
 
   constructor(private schedulesService: SchedulesService) {}
 
+
   ngOnInit() {
-    console.log(this.schedulesService.schedules)
-    this.schedules = this.schedulesService.schedules
+    console.log('Init',this.schedulesService.schedules)
+    this.scheduleLinks = this.schedulesService.schedules
   }
 
+  ngOnChanges() {
+    console.log('Change',this.schedulesService.schedules)
+  }
+
+  onAddSchedule() {
+    if (this.scheduleName) {
+      console.log('asd', this.scheduleName)
+      this.schedulesService.addSchedule(this.scheduleName)
+    }
+  }
 }
